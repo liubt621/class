b2f620b213b7effbcf68b26580102452c3f8f9f8
liubt621 <liuuul0621@gmail.com>
2023-06-05 10:18:28 +0800

    first commit

---
+++ c.py
@@ -0,0 +1,8 @@
+class student:
+	def __init__(self,name,age):
+		self.name = name
+		self.age = age
+a1 = student("Alice",25)
+print ("hello,my name is",a1.name)
+print ("I am ",a1.age,"years old")
+
